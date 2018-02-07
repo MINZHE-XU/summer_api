0373f85d5c01192f2fd12a75ea56a5b278e76532
MINZHE-XU <mazin.xu@gmail.com>
2018-02-07 22:34:09 +1100

    aaa

--- server.js
+++ server.js
@@ -146,4 +146,15 @@ console.log('Magic happens on port ' + port);
 
 var mongoose   = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/map'); // connect to our database
+
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to ' );
+});
+mongoose.connection.on('error',function (err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected');
+});
+
 var Datagroup     = require('./app/models/datagroup');
